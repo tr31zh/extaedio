a66143ac9c5294514f46a6370ab05fd1e482d154
Felicià MAVIANE <maviane@gmail.com>
2020-06-15 15:31:58 +0200

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.streamlit/
-echo “\
+
+echo "\
 [general]\n\
-email = \”maviane@gmail.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+email = \"maviane@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
